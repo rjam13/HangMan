ad0237c2ea0bb3d3602d35f5fab14117669d9f8f
rjam13 <reymarasigan001@gmail.com>
2021-09-21 16:35:22 -0400

    9/21

--- HangMan/ViewController.swift
+++ HangMan/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 //UITextFieldDelegate for applying text length limit
 class ViewController: UIViewController, UITextFieldDelegate {
+    //HangMan himself
+//    var head: UIImageView!
+//    var torso: UIImageView!
+//    var rightArm: UIImageView!
+//    var leftArm: UIImageView!
+//    var rightLeg: UIImageView!
+//    var leftLeg: UIImageView!
+    
     //UI labels
     var word: UILabel!
     var userAnswer: UITextField!
@@ -16,20 +24,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var scoreLabel: UILabel!
     var score = 0 {
         didSet {
-        scoreLabel.text = "Score: \(score)"
+            scoreLabel.text = "Score: \(score)"
         }
     }
     var mistakeLabel: UILabel!
     var mistakes = 0 {
         didSet {
-        mistakeLabel.text = "Mistakes: \(mistakes) / 6"
+            mistakeLabel.text = "Mistakes: \(mistakes) / 6"
+        }
+    }
+    var highScoreLabel: UILabel!
+    var highScore = 0 {
+        didSet {
+            highScoreLabel.text = "High Score: \(highScore)"
         }
     }
     var letterView: UIView!
     var letterViewField: UILabel!
     
-    //used for spliting the questionMarks into a characters array that can replace characters
-    var partsOfquestionMarks = [Character]()
+    //used for replacing question marks index by index into characters
+    var questionMarks = [Character]()
     //the solution split into characters array to check
     var partsOfsolution = [Character]()
     
@@ -48,6 +62,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         loadView()
         startGame()
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedScore = defaults.object(forKey: "highScore") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                highScore = try jsonDecoder.decode(Int.self, from: savedScore)
+            } catch {
+                print("Failed to load high score")
+            }
+            
+        }
     }
     
     func startGame() {
@@ -55,13 +82,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         score = 0
         currentLetterUsed = 0
         usedWords = [""]
-        loadWords()
+        loadData()
         loadLevel()
     }
     
-    func loadWords() {
+    func loadData() {
+        let path = Bundle.main
+        
         //makes txt file into a url type
-        if let wordsFile = Bundle.main.url(forResource: "start", withExtension: "txt") {
+        if let wordsFile = path.url(forResource: "start", withExtension: "txt") {
             //the wordsFile url is turned into a single string
             if let wordsContent = try? String(contentsOf: wordsFile) {
                 //the wordsContent, seperated by \n, is turned into an array of Strings
@@ -88,14 +117,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         //keeps track of all the places of each letter in solution
         partsOfsolution = Array(solution)
-        partsOfquestionMarks.removeAll()
+        questionMarks.removeAll()
         
         //sets the word UILabel with question marks that is as long as solution.count
         for _ in 0...solution.count - 1 {
-            partsOfquestionMarks.append("?")
+            questionMarks.append("?")
         }
 
-        word.text = String(partsOfquestionMarks)
+        word.text = String(questionMarks)
     }
     
     override func loadView() {
@@ -147,6 +176,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mistakeLabel.text = "Mistakes: \(mistakes) / 6"
         view.addSubview(mistakeLabel)
         
+        highScoreLabel = UILabel()
+        highScoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        highScoreLabel.textAlignment = .center
+        highScoreLabel.font = UIFont.systemFont(ofSize: 20)
+        highScoreLabel.textColor = .black
+        highScoreLabel.text = "High Score: \(highScore)"
+        view.addSubview(highScoreLabel)
+        
+  // working progress
+//        head = UIImageView(image: UIImage(contentsOfFile: "head"))
+//        torso = UIImageView(image: UIImage(contentsOfFile: "part"))
+//        leftArm = UIImageView(image: UIImage(contentsOfFile: "part"))
+//        rightArm = UIImageView(image: UIImage(contentsOfFile: "part"))
+//        leftLeg = UIImageView(image: UIImage(contentsOfFile: "part"))
+//        rightLeg = UIImageView(image: UIImage(contentsOfFile: "part"))
+        
         NSLayoutConstraint.activate([
             word.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             word.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -100),
@@ -162,6 +207,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
             
             mistakeLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
             mistakeLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10),
+            
+            highScoreLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            highScoreLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10),
+            
+            //HangMan himself
+//            head.topAnchor.constraint(equalTo: scoreLabel.topAnchor),
+//            torso.topAnchor.constraint(equalTo: head.bottomAnchor),
+//            leftLeg.topAnchor.constraint(equalTo: torso.bottomAnchor),
+//            leftLeg.rightAnchor.constraint(equalTo: torso.leftAnchor),
+//            rightLeg.topAnchor.constraint(equalTo: leftLeg.topAnchor),
+//            rightLeg.leftAnchor.constraint(equalTo: torso.rightAnchor),
+//            leftArm.leftAnchor.constraint(equalTo: leftLeg.rightAnchor),
+//            leftArm.topAnchor.constraint(equalTo: head.bottomAnchor),
+//            rightArm.leftAnchor.constraint(equalTo: rightLeg.rightAnchor),
+//            rightArm.topAnchor.constraint(equalTo: head.bottomAnchor),
+            
+            
         ])
         
         loadLetters()
@@ -197,10 +259,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
             // calculate the frame of this button using its letterPos and row
             let frame = CGRect(x: (letterPos % 6 + 1) * width, y: row * height, width: width, height: height)
             letter.frame = frame
-            
-//            if letterPos == 6 {
-//                row += 1
-//            }
 
             // add letter to the letterView
             lettersUsed.append(letter)
@@ -213,7 +271,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
             letterView.topAnchor.constraint(equalTo: submit.bottomAnchor, constant: 80),
             letterView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
             
-            letterViewField.centerXAnchor.constraint(equalTo: letterView.centerXAnchor)
+            letterViewField.centerXAnchor.constraint(equalTo: letterView.centerXAnchor),
+            letterViewField.topAnchor.constraint(equalTo: letterView.topAnchor, constant: 5)
         ])
         
     }
@@ -228,31 +287,46 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if partsOfsolution.contains(character) {
             
             for (index, item) in partsOfsolution.enumerated() {
-                //using the index where the character is in solution, replace the question mark of that same index in partsOfquestionMarks with character if character == item
+                //using the index where the character is in solution, replace the question mark of that same index in questionMarks with character if character == item
                 if character == item {
-                    partsOfquestionMarks[index] = character
+                    questionMarks[index] = character
                 }
             }
             
-            word.text = String(partsOfquestionMarks)
+            word.text = String(questionMarks)
             
         //increments mistakes if character does not exist within word
         } else {
             
+            
             //once the mistakes reach 6, the game starts over
             if mistakes == 5 {
-                let ac = UIAlertController(title: "RIP", message: "Game Over. Your final score is: \(score)", preferredStyle: .alert)
-                ac.addAction(UIAlertAction(title: "start over", style: .default) {
-                    [weak self] _ in
-                    self?.letterView.removeFromSuperview()
-                    self?.loadLetters()
-                    self?.startGame()
-                })
-                present(ac, animated: true)
+                
+                //changes high score once the current score passes it.
+                if(score >= highScore) {
+                    let ac = UIAlertController(title: "New high score!: \(score)", message: "Game Over. The word was \(String(partsOfsolution))", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "start over", style: .default) {
+                        [weak self] _ in
+                        self?.letterView.removeFromSuperview()
+                        self?.loadLetters()
+                        self?.startGame()
+                    })
+                    present(ac, animated: true)
+                    
+                } else {
+                    let ac = UIAlertController(title: "Final Score: \(score)", message: "Game Over. The word was \(String(partsOfsolution))", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "start over", style: .default) {
+                        [weak self] _ in
+                        self?.letterView.removeFromSuperview()
+                        self?.loadLetters()
+                        self?.startGame()
+                    })
+                    present(ac, animated: true)
+                }
             }
             
             mistakes += 1
-            let ac = UIAlertController(title: "nope", message: nil, preferredStyle: .alert)
+            let ac = UIAlertController(title: "Nope", message: nil, preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
             present(ac, animated: true)
             
@@ -263,15 +337,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
         
         //if the word is fully guessed
-        if !partsOfquestionMarks.contains("?") {
+        if !questionMarks.contains("?") {
             score += 1
+            
             let ac = UIAlertController(title: "Nice!", message: "Good job! Ready for next level?", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "yes", style: .default) {
+            ac.addAction(UIAlertAction(title: "OK", style: .default) {
                 [weak self] _ in
                 self?.loadLevel()
             })
             present(ac, animated: true)
             
+            if(score > highScore) {
+                highScore = score
+                save()
+            }
             mistakes = 0
             letterView.removeFromSuperview()
             loadLetters()
@@ -291,6 +370,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    //saving the high score
+    func save() {
+        let JsonEncoder = JSONEncoder()
+        
+        if let savedData = try? JsonEncoder.encode(highScore) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "highScore")
+        } else {
+            print("Failed to save high score")
+        }
     }
 
 }
